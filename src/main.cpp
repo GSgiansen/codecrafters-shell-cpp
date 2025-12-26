b9f24334b7a26aa5e8d6a423b115799b5a6d3913
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-12-26 15:28:03 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,35 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <filesystem>
+#include <cstdlib>
+
+namespace fs = std::filesystem;
+std::string find_in_path(const std::string cmd) {
+  const char* path_env = std::getenv("PATH");
+  if (path_env == nullptr) return "";
+
+  std::stringstream ss(path_env);
+  std::string directory;
+
+  while (getline(ss, directory, ':')) {
+    std::filesystem::path full_path = std::filesystem::path(directory) / cmd;
+    if (std::filesystem::exists(full_path)) {
+      auto perms = std::filesystem::status(full_path).permissions();
+      bool is_executable = ((perms & fs::perms::owner_exec) != fs::perms::none ||
+                            (perms & fs::perms::group_exec) != fs::perms::none ||
+                            (perms & fs::perms::others_exec) != fs::perms::none);
+
+      if (is_executable) {
+        return full_path.string();
+      }
+
+    }
+  }
+  return "";
+
+
+}
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
@@ -32,9 +61,18 @@ int main() {
         ss >> word;
         if (std::find(builtins.begin(), builtins.end(), word) != builtins.end()) {
             std::cout << word << " is a shell builtin\n";
-        } else {
-          std::cout << word << ": not found\n";
+        } 
+        else {
+          // scan through each directory that can be 
+          std::string full_path = find_in_path(word);
 
+          if (!full_path.empty()) {
+            std::cout << word << " is " << full_path << "\n";
+          }
+          else {
+            
+            std::cout << word << ": not found\n";
+          }
         }
 
       }
