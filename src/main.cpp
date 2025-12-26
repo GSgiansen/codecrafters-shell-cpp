0dcbb41c3af05ff26ee5a07d80de80c7671dc80f
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-12-26 14:38:33 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -10,17 +10,30 @@ int main() {
     std::cout << "$ ";
     std::string command;
     std::getline(std::cin, command);
+    std::string builtins[3] = {"type", "echo", "exit"};
 
     if (!command.empty()) {
       std::string word;
       std::stringstream ss(command);
       ss >> word;
+
       if (word  == "exit") return 0;
+
       else if (word  == "echo") {
         while (ss >> word) {
           std::cout << word << " ";
         }
         std::cout << "\n";
+      }
+
+      else if (word == "type") {
+        ss >> word;
+        if (std::find(builtins.begin(), builtins.end(), word != builtsin.end()) {
+            std::cout << word << " is a shell builtin\n";
+        } else {
+          std::co9ut << worrd << ": not found";
+
+        }
 
       }
       else {
