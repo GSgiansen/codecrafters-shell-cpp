e271dac4e176f94f04043e71c92846493476ad66
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2025-12-26 14:11:47 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,14 @@ int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+  while (true) {
 
-  std::cout << "$ ";
-  std::string command;
-  std::getline(std::cin, command);
+    std::cout << "$ ";
+    std::string command;
+    std::getline(std::cin, command);
 
-  if (!command.empty()) {
-    std::cout << command << ": command not found\n";
+    if (!command.empty()) {
+      std::cout << command << ": command not found\n";
+    }
   }
 }
