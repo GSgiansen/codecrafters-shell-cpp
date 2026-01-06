b869ac92cbfa97a633bd899bf4f6acbcccba50e6
Tan Gian Sen <64723554+GSgiansen@users.noreply.github.com>
2026-01-06 21:57:18 +0800

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -64,10 +64,20 @@ void handle_pwd() {
 }
 
 void handle_cd(std::string newPath) {
-  try {
-    fs::current_path(newPath);
-  } catch(fs::filesystem_error const& e) {
-      std::cerr << "cd: " << newPath << ": No such file or directory\n";
+  const char* path_env = std::getenv("HOME");
+  std::string cpp_home = path_env;
+  if (newPath == "~") {
+    try {
+      fs::current_path(cpp_home);
+    } catch (fs::filesystem_error const& e) {
+    }
+  }
+  else {
+    try {
+      fs::current_path(newPath);
+    } catch(fs::filesystem_error const& e) {
+        std::cerr << "cd: " << newPath << ": No such file or directory\n";
+    }
   }
 }
 
